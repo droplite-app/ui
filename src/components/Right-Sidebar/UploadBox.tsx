8c12f375996f07bbeb3dadfc8147c5581d8067db
Bahar Işıklı <baharisikli@outlook.com>
2024-08-24 17:32:47 +0300

    bug fixes

--- src/components/Right-Sidebar/UploadBox.tsx
+++ src/components/Right-Sidebar/UploadBox.tsx
@@ -1,4 +1,5 @@
 import UploadBoxIcon from "../../assets/Icons/UploadBoxIcon";
+
 const UploadBox = () => {
   return (
     <div className="w-[350px] h-[60px] bg-blue-50 mx-auto rounded-[20px] mt-[20px] pl-[10px] flex flex-col justify-center items-center 
@@ -6,8 +7,11 @@ const UploadBox = () => {
     md:w-[15px] md:h-[40px] md:bg-blue-50 md:border-gray-300 md:border-solid 
     lg:w-[350px] lg:h-[360px] lg:bg-blue-50 lg:border-gray-300 lg:border-solid">
       
-      <UploadBoxIcon/>
+      <UploadBoxIcon />
+
+     
       
+
     </div>
   );
 };
