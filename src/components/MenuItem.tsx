147c327f8ab8f4e52a8116f4d11bdb9be290146e
Bahar Işıklı <baharisikli@outlook.com>
2024-08-24 13:21:10 +0300

    used classnames in the conditions

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 
 interface MenuItemProps {
   icon: React.ReactNode;
@@ -13,19 +14,23 @@ const MenuItem: React.FC<MenuItemProps> = ({
   href,
   isActive = false,
 }) => {
-  return (
-    <li
-      className={`flex items-center w-[307px] h-[69px] px-5 py-4 gap-4 ${
-        isActive ? "bg-sky-900" : "bg-sky-900"
-      } text-white hover:bg-sky-700 transition-colors`}
-    >
-      <a href={href} className="flex items-center text-white no-underline ">
-        <span>{icon}</span>
-      </a>
+// We are using the classNames library to combine class names
+const liClassName = classNames(
+  "flex items-center w-[307px] h-[69px] px-5 py-4 gap-4 text-white hover:bg-sky-700 transition-colors",
+  {
+    "bg-sky-900": isActive, // Conditionally adds the background color
 
-      <span className="text-white">{text}</span>
-    </li>
-  );
+  }
+);
+
+return (
+  <li className={liClassName}>
+    <a href={href} className="flex items-center text-white no-underline ">
+      <span>{icon}</span>
+    </a>
+    <span className="text-white">{text}</span>
+  </li>
+);
 };
 
 export default MenuItem;
