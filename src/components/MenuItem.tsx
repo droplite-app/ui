6e02fc95cc6b16a6d41034ed073bb813fcd5c61d
Bahar Işıklı <baharisikli@outlook.com>
2024-08-24 13:25:37 +0300

    removed useless ısActive

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -8,29 +8,20 @@ interface MenuItemProps {
   isActive?: boolean;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({
-  icon,
-  text,
-  href,
-  isActive = false,
-}) => {
-// We are using the classNames library to combine class names
-const liClassName = classNames(
-  "flex items-center w-[307px] h-[69px] px-5 py-4 gap-4 text-white hover:bg-sky-700 transition-colors",
-  {
-    "bg-sky-900": isActive, // Conditionally adds the background color
+const MenuItem: React.FC<MenuItemProps> = ({ icon, text, href }) => {
+  // We are using the classNames library to combine class names
+  const liClassName = classNames(
+    "flex items-center w-[307px] h-[69px] px-5 py-4 gap-4 bg-sky-900 text-white hover:bg-sky-700 transition-colors",
+  );
 
-  }
-);
-
-return (
-  <li className={liClassName}>
-    <a href={href} className="flex items-center text-white no-underline ">
-      <span>{icon}</span>
-    </a>
-    <span className="text-white">{text}</span>
-  </li>
-);
+  return (
+    <li className={liClassName}>
+      <a href={href} className="flex items-center text-white no-underline ">
+        <span>{icon}</span>
+      </a>
+      <span className="text-white">{text}</span>
+    </li>
+  );
 };
 
 export default MenuItem;
