448a501c374292f60dcad49820963a5d182badc4
Bahar Işıklı <baharisikli@outlook.com>
2024-08-25 11:50:43 +0300

    upload box repositioned

--- src/components/Right-Sidebar/UploadBox.tsx
+++ src/components/Right-Sidebar/UploadBox.tsx
@@ -1,17 +1,22 @@
 import UploadBoxIcon from "../../assets/Icons/UploadBoxIcon";
 
 const UploadBox = () => {
+  const handleClick = () => {
+    //this will be add up later
+  };
+
   return (
-    <div className="w-[350px] h-[60px] bg-blue-50 mx-auto rounded-[20px] mt-[20px] pl-[10px] flex flex-col justify-center items-center 
+    <div
+      className="w-[350px] h-[60px] bg-blue-50 mx-auto rounded-[20px] mt-[20px] pl-[10px] flex flex-col justify-center items-center 
     border border-dotted border-sky-300 
     md:w-[15px] md:h-[40px] md:bg-blue-50 md:border-gray-300 md:border-solid 
-    lg:w-[350px] lg:h-[360px] lg:bg-blue-50 lg:border-gray-300 lg:border-solid">
-      
-      <UploadBoxIcon />
+    lg:w-[350px] lg:h-[360px] lg:bg-blue-50 lg:border-gray-300 lg:border-solid"
+    >
+      <UploadBoxIcon onClick={handleClick} />
 
-     
-      
-
+      <p className="mt-0 pb-3 text-xs md:mt-2 md:pb-0 md:text-lg lg:mt-5 lg:text-2xl font-bold text-sky-900">
+        Upload File
+      </p>
     </div>
   );
 };
