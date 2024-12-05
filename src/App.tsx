e3c696553bdf38b745c1150fa88c2872c022208f
Bahar Işıklı <baharisikli@outlook.com>
2024-12-05 15:34:03 +0300

    OFB-51 solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
 import Main from "./pages/Main";
+import React, { ReactNode } from "react";
 
-function App() {
+const isAuthenticated = (): boolean => {
+  return !!localStorage.getItem("userToken");
+};
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  return isAuthenticated() ? <>{children}</> : <Navigate to="/login" />;
+};
+
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
@@ -12,10 +30,17 @@ function App() {
         <Route path="/create-account" element={<CreateAccount />} />
 
         {/* Main page layout */}
-        <Route path="*" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Main />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
