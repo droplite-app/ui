def8fde5ad2c7f5f37725e3a444692e61cd588a5
Bahar Işıklı <baharisikli@outlook.com>
2024-12-30 20:47:28 +0300

    redesigning app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
+  createBrowserRouter,
   Navigate,
+  RouterProvider,
 } from "react-router-dom";
 import Login from "./pages/Login";
 import CreateAccount from "./pages/CreateAccount";
@@ -26,35 +25,34 @@ const PublicRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   return !isAuthenticated() ? <>{children}</> : <Navigate to="/" />;
 };
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Main />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <PublicRoute>
+        <Login />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* Independent Routes */}
-        <Route
-          path="/login"
-          element={
-            <PublicRoute>
-              <Login />
-            </PublicRoute>
-          }
-        />
-        <Route path="/create-account" element={<CreateAccount />} />
-
-        {/* Main page layout */}
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Main />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 };
-
 export default App;
